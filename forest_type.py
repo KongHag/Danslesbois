246defb57de26d0a4423ba2859d2e967decfeb60
hugof38 <39108853+hugof38@users.noreply.github.com>
2018-09-09 21:15:02 +0200

    init

---
+++ forest_type.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  9 19:13:47 2018
+
+@author: hugof
+"""
+
+import pandas as pd
+import numpy as np
+
+
+train = pd.read_csv(r'C:\Users\hugof\OneDrive\Documents\Kaggle\all\train.csv',sep=',')
+test = pd.read_csv(r'C:\Users\hugof\OneDrive\Documents\Kaggle\all\test.csv',sep=',')
+
